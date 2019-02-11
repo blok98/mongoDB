1176bbcc1357e988a5f2ef925dc6f4bc973a0890
illprogramtheshitoutofyou <43383986+illprogramtheshitoutofyou@users.noreply.github.com>
2019-02-11 07:51:04 +0100

    Update code.py

--- code.py
+++ code.py
@@ -7,29 +7,40 @@ db                 = client['mijndb']
 collection_student = db['products']
 data               = collection_student.find()
 
-customerNumber      = 1
-customer_info    = data[0]
+def data_list():
+    data_list=[]
+    for value in data:
+        data_list.append(value)
+    return data_list
+
+def customer_info():
+    customerNumber      = 1
+    customer_info    = data_list[0]
+    name                = customer_info['name']
+    selling_price       = customer_info['price']['selling_price']
+    return name,selling_price
+
+def name_with_R():
+    naamMetR='geen naam gevonden'
+    for value in data_list:
+        name        = value['name']
+        if name[0] == 'R':
+            naamMetR= name
+            break
+    return naamMetR
+
+def average_price():
+    totPrice=0
+    count=0
+    for value in data_list:
+        count+=1
+        selling_price = value['price']['selling_price']
+        totPrice +=selling_price
+    average=totPrice/count
+    return average
 
 
-name                = customer_info['name']
-selling_price       = customer_info['price']['selling_price']
-
-print('name: ',name)
-print('selling_price: ', selling_price)
-
-
-totPrice=0
-count=0
-for value in data:
-    count+=1
-    name          =  value['name']
-    selling_price =  value['price']['selling_price']
-    totPrice      += selling_price
-    if name[0]    == 'R':
-        naamMetR  =  name
-
-avPrice           = totPrice/count
-print('\n')
-print('eerste naam met R: ',naamMetR)
-print('\n')
-print('gemiddelde prijs van producten: ',avPrice)
+data_list=data_list()
+print(customer_info())
+print(name_with_R())
+print(average_price())
